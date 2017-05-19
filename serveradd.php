094b0210d304a06129ec7a6898985b775efe3bd4
danmed <danmed@gmail.com>
2017-05-19 14:22:38 +0100

    Update serveradd.php

--- serveradd.php
+++ serveradd.php
@@ -3,9 +3,15 @@
 <title>
 PStatus - Add Server
 </title>
+<style>
+body
+{
+font-family:courier,serif
+}
+</style>
 </head>
 <body>
 <center>
 <table cellpadding="4" cellspacing="4" border="1">
-<tr><td colspan="4">Add Server</td></tr>
+<tr><td colspan="4"><center>Add Server</td></tr>
 <tr><td>Name</td><td>IP Address</td><td>Info</td><td>Purpose</td></tr>
