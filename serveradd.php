e89e410e5e7954909a86762970c5cfb0636301d8
danmed <danmed@gmail.com>
2017-05-19 14:21:32 +0100

    Create serveradd.php

---
+++ serveradd.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>
+PStatus - Add Server
+</title>
+</head>
+<body>
+<center>
+<table cellpadding="4" cellspacing="4" border="1">
+<tr><td colspan="4">Add Server</td></tr>
+<tr><td>Name</td><td>IP Address</td><td>Info</td><td>Purpose</td></tr>
