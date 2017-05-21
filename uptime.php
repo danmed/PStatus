ff9e659cec1c047cc3a211bedc0060b1ee6a3054
danmed <danmed@gmail.com>
2017-05-21 23:29:38 +0100

    Update uptime.php

--- uptime.php
+++ uptime.php
@@ -10,13 +10,25 @@ while ($db_field = mysqli_fetch_assoc($result))
 {
 	$id = $db_field['id'];
   	$ip = $db_field['ip'];
-	$SQL2 = "UPDATE uptime SET count = count + 1 WHERE parent = '" . $id . "'";
+	$online  = pingtest($ip);
+	if ($online == true){
+	$SQL2 = "UPDATE uptime SET count = count + 1, ups = ups + 1 WHERE parent = '" . $id . "'";
+	}
+	else
+	{
+	$SQL2 = "UPDATE uptime SET count = count + 1, downs = downs + 1 WHERE parent = '" . $id . "'";
+	}
 	echo $SQL2;
 if (mysqli_query($db_handle, $SQL2)) {
     echo "Record updated successfully";
 } else {
     echo "Error updating record: " . mysqli_error($db_handle);
 }
+}
+	function pingtest($ip) {
+    
+	exec(sprintf('ping -c 1 -W 5 %s', escapeshellarg($ip)), $errorNo, $errorStr);
+	return $errorStr === 0;
 }
 }	
 	
