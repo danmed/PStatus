d269f250b5e36c4e1ac137ecc296528f56c59b09
danmed <danmed@gmail.com>
2017-05-21 23:30:31 +0100

    Update uptime.php

--- uptime.php
+++ uptime.php
@@ -11,7 +11,7 @@ while ($db_field = mysqli_fetch_assoc($result))
 	$id = $db_field['id'];
   	$ip = $db_field['ip'];
 	$online  = pingtest($ip);
-	if ($online == true){
+	if ($online == 1){
 	$SQL2 = "UPDATE uptime SET count = count + 1, ups = ups + 1 WHERE parent = '" . $id . "'";
 	}
 	else
