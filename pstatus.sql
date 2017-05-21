67cd5b72f47264996330c76d0f27821c763e4bbe
danmed <danmed@gmail.com>
2017-05-22 00:03:16 +0100

    Added uptime table
    Uptime table allows accumulation of uptime data via uptime.php run as a cront timer.

--- pstatus.sql
+++ pstatus.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 19, 2017 at 02:36 PM
+-- Generation Time: May 21, 2017 at 11:02 PM
 -- Server version: 5.6.35-1+deb.sury.org~xenial+0.1
 -- PHP Version: 7.0.15-0ubuntu0.16.04.4
 
@@ -75,6 +75,22 @@ CREATE TABLE `smartdevices` (
   `purpose` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uptime`
+--
+
+CREATE TABLE `uptime` (
+  `id` int(11) NOT NULL,
+  `parent` int(11) NOT NULL,
+  `count` int(11) NOT NULL,
+  `ups` int(11) NOT NULL,
+  `downs` int(11) NOT NULL,
+  `lastup` datetime NOT NULL,
+  `lastdown` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -98,6 +114,12 @@ ALTER TABLE `smartdevices`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `uptime`
+--
+ALTER TABLE `uptime`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -105,17 +127,22 @@ ALTER TABLE `smartdevices`
 -- AUTO_INCREMENT for table `servers`
 --
 ALTER TABLE `servers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT for table `smartdevices`
 --
 ALTER TABLE `smartdevices`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `uptime`
+--
+ALTER TABLE `uptime`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
