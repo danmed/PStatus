19c60af65f211e1f86381cf52797d25f016f41cb
danmed <danmed@gmail.com>
2017-05-21 23:00:56 +0100

    Create uptime.php

---
+++ uptime.php
@@ -0,0 +1,24 @@
+<?PHP
+include "config.inc.php";
+$db_handle = mysqli_connect($DBServer, $DBUser, $DBPassword);
+$db_found = mysqli_select_db($db_handle, 'status');
+if ($db_found) 
+{
+$SQL = "select * from servers"	;
+$result = mysqli_query($db_handle, $SQL);
+while ($db_field = mysqli_fetch_assoc($result))
+{
+	$id = $db_field['id'];
+  $ip = $db_field['ip'];
+	$online  = pingtest($ip);
+	$SQL2 = "update uptime SET count = count + 1 WHERE parent = $id";
+  mysqli_query($db_handle, $sql);
+}
+
+
+function pingtest($ip) {
+    
+	exec(sprintf('ping -c 1 -W 5 %s', escapeshellarg($ip)), $errorNo, $errorStr);
+	return $errorStr === 0;
+}
+?>
