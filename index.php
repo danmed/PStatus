3faab002f70da69b56cab3c1878b1a448fa57af1
danmed <danmed@gmail.com>
2017-05-22 16:16:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -35,10 +35,10 @@ $(document).ready(function(){
 </script>
 </head>
 <body>
-<Center>
+<Center><form method="GET" name="refr" action="<?php echo $_SERVER['PHP_SELF'];?>">
 <table cellpadding="4" cellspacing="4" border="1">
 	<tr>
-		<td><a href="serveradd.php">Admin</a></td><td><form method="GET" name="refr" action="<?php echo $_SERVER['PHP_SELF'];?>"><select name="refresh" value="refresh" onchange="this.form.submit()"><option>refresh</option><option value="5">5</option><option value="10">10</option><option value="15">15</option><option value="20">20</option><option value="25">25</option><option value="30">30</option></td></tr></table></form><br>
+		<td><a href="serveradd.php">Admin</a></td><td><select name="refresh" value="refresh" onchange="this.form.submit()"><option>refresh</option><option value="5">5</option><option value="10">10</option><option value="15">15</option><option value="20">20</option><option value="25">25</option><option value="30">30</option></td></tr></table></form><br>
 	
 <table id="status" cellpadding="4" cellspacing="4" border="1">
 <tr><td colspan="5"><center><b>Server Ping Status</td></tr>
