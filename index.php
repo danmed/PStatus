25d4501e32764924f591cda735ea95a3316880fc
danmed <danmed@gmail.com>
2017-05-22 09:53:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ while ($db_field = mysqli_fetch_assoc($result))
 
 ?>
 <tr><td colspan="5"><center><b>Smart Device Ping Status</td></tr>
-<tr><td><b>DEVICE</td><td><b>INFO</td><td><b colspan="2">PURPOSE</td><td><b>STATUS</td></tr>
+<tr><td><b>DEVICE</td><td><b>INFO</td><td colspan="2"><b>PURPOSE</td><td><b>STATUS</td></tr>
 <?PHP
 
 $SQL = "select * from smartdevices"	;
