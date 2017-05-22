f62aa524117d5177363d91b2e107d0549a52acf8
danmed <danmed@gmail.com>
2017-05-22 11:18:48 +0100

    Update stats.php

--- stats.php
+++ stats.php
@@ -38,17 +38,16 @@ PStatus Statistics
     min: 0,
     max: <?PHP echo $max; ?>,
     title: "<?PHP echo $device; ?>",
-    levelColorsGradient: false'
-	  customSectors: {
-        percents: true,
-        ranges: [{
-          color : "#43bf58",
-          lo : 0,
-          hi : 50
-        },{
-          color : "#ff3b30",
-          lo : 51,
-          hi : 100
+    customSectors: {
+    percents: true,
+    ranges: [{
+    color : "#43bf58",
+    lo : 0,
+    hi : 50
+    },{
+    color : "#ff3b30",
+     lo : 51,
+      hi : 100
         }]
       }
   });
