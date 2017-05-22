4d74322dc69111281b75ced7a3901a09486b0361
danmed <danmed@gmail.com>
2017-05-22 16:20:17 +0100

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,9 +1,17 @@
 <?PHP
 
-# MYSQL Configuration
-
+/* MYSQL Configuration */
 $DBServer	= 	'192.168.2.75';
 $DBUser		= 	'kodi';
 $DBPassword     = 	'kodi';
+
+/* Default refresh rate for index page */
+$Refresh = '20';
+
+/* Email Configuration */
+$smtp = "smtp.gmail.com";
+$port = "587";
+$username = "testtest@gmail.com";
+$password = "testtest";
 
 ?>
