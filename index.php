110a97c2394ddb78af25dbd1be578a11d1f0dede
danmed <danmed@gmail.com>
2017-05-23 14:13:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -189,5 +189,21 @@ function showPage() {
 <a href="#">Clients</a>
 <a href="#">Contact</a>
 </div>
+	
+<script>
+	/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
+	</script>
 	</body>
 	</html>
