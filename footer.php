49f4e07fbd1e8358d24ce1bad7cc9f923bd61004
danmed <danmed@gmail.com>
2017-05-23 10:30:48 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,4 @@
+<?PHP
+echo "<font size=1>Design and code by Dan Medhurst 2017</font><br>";
+echo "<font size=1>Icons made by <a href='http://www.flaticon.com/authors/icon-works'>Icon Works</a> from <a href='http://www.flaticon.com'>www.flaticon.com</a></font>";
+?>
