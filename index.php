cbbd24c034a35ad7135d1ea85c783168e76876f4
danmed <danmed@gmail.com>
2017-05-23 14:14:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -191,18 +191,14 @@ function showPage() {
 </div>
 	
 <script>
-	/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+/* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+/* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
 }
 	</script>
 	</body>
