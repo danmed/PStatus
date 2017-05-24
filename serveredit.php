1f524c4c15a94db81b84c7a02d6f7011517baf25
danmed <danmed@gmail.com>
2017-05-24 13:56:10 +0100

    Update serveredit.php

--- serveredit.php
+++ serveredit.php
@@ -17,7 +17,7 @@
   <?PHP include "navbar.php"; ?>
 
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 
 <center>
