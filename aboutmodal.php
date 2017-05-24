73a7551812bb501bf061bfdafc97132feb3d1f4c
danmed <danmed@gmail.com>
2017-05-24 14:41:38 +0100

    Create aboutmodal.php

---
+++ aboutmodal.php
@@ -0,0 +1,21 @@
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">About PStatus</h4>
+      </div>
+      <div class="modal-body">
+        <p>Written by Dan Medhurst</p>
+        <p>https://github.com/danmed/PStatus</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
