8877a400f116abc69692ed7e16d335501eb266a1
danmed <danmed@gmail.com>
2017-05-24 14:43:02 +0100

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -11,7 +11,7 @@
 		</div>
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#">About</a></li>
+				<li><a href="#" data-toggle="modal" data-target="#aboutmodal">About</a></li>
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Refresh<span class="caret"></span></a>
 					<ul class="dropdown-menu">
