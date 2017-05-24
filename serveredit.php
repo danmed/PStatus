94a46a0f848ed06667f26f24b2af32a887c0521d
danmed <danmed@gmail.com>
2017-05-24 09:50:40 +0100

    Update serveredit.php

--- serveredit.php
+++ serveredit.php
@@ -11,15 +11,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  <style>
-  .progress {
-    margin-bottom: 0 !important;
-	background-color: #DA2A2A;
-    -webkit-box-shadow: none;
-    box-shadow: none;
-}
-</style>
-	</head>
+</head>
 
   <body>
 
@@ -97,6 +89,7 @@ while ($db_field = mysqli_fetch_assoc($result))
 <tr><td><input type="text" size="20" name="device" value="<?PHP echo $device; ?>"</td><td><input type="text" size="20" name="ip" value="<?PHP echo $ip;?>"</td><td><input type="text" size="20" name="info" value="<?PHP echo $info; ?>"</td><td><input type="text" size="20" name="purpose" value="<?PHP echo $purpose; ?>"</td><td><input type="submit" value="update></td><td>RESET</td></tr>
 
   </form>
+<?PHP
  }
  }
  ?>
