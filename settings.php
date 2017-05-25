cce601a830fc959f4a5df86074255565afeda6d6
danmed <danmed@gmail.com>
2017-05-25 13:58:18 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -39,7 +39,9 @@ include "config.inc.php";
 
 			
 <table class="table table-striped" cellpadding="4" cellspacing="4" border="1">
+	<thead>
 	<tr><th colspan="2"><center><img src="icons/email-outbox.png">&nbsp;Misc Settings</th></tr>
+	</thead>
 	<tr><Td><b>Enable Smart Devices</td><td><b>Default Refresh Rate</td></tr>
 	<tr><td><input type="checkbox" name="enable_smart" value="1" <?php if($enable_smart === '1') echo 'checked="checked"';?>></td><td><input type="text" size="20" name="refresh" value="<?PHP echo $refresh; ?>"></td></tr>
 	</table>
