065933b6d6fd81191049552b97bf96af134d9314
danmed <danmed@gmail.com>
2017-05-25 11:36:23 +0100

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -5,14 +5,26 @@ $DBServer	= 	'192.168.2.75';
 $DBUser		= 	'kodi';
 $DBPassword     = 	'kodi';
 
+$db_handle = mysqli_connect($DBServer, $DBUser, $DBPassword);
+$db_found = mysqli_select_db($db_handle, 'status');
+if ($db_found) 
+{
+$config_sql = "select * from config"	;
+$config_result = mysqli_query($db_handle, $config_sql);
+while ($db_field = mysqli_fetch_assoc($config_result))
+{
+/* Display Smart Devices */
+$enable_smart = $db_field['smartenable'];
 /* Default refresh rate for index page */
-$refresh = '20';
-
-/* Email Configuration */
-$smtp = 'smtp.gmail.com';
-$port = '587';
-$username = 'testtest@gmail.com';
-$password = 'testtest';
-$adminemail = 'admin@yourdomain.com';
+$refresh = $db_field['refresh'];
+/* SMTP Details for admin emails */
+$smtp = $db_field['smtp'];
+$smtp_port = $db_field['smtp_port'];
+$smtp_username = $db_field['smtp_username'];
+$smtp_username = $db_field['smtp_username'];
+$smtp_password = $db_field['smtp_password'];
+$admin_email = $db_field['admin_email'];
+}
+}
 
 ?>
