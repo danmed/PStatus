378e5c7006f10130b5278e26c75d8a18478b24a2
danmed <danmed@gmail.com>
2017-05-25 11:24:05 +0100

    Update serveredit.php

--- serveredit.php
+++ serveredit.php
@@ -86,7 +86,7 @@ while ($db_field = mysqli_fetch_assoc($result))
  ?>
 
 <form method="POST" action="serveredit.php">
-<input type="hidden" value="updateserver" name="<?PHP echo $id; ?>">
+<input type="hidden" value="<?PHP echo $id; ?>" name="updateserver">
 <tr><td><input type="text" size="20" name="device" value="<?PHP echo $device; ?>"</td><td><input type="text" size="20" name="ip" value="<?PHP echo $ip;?>"</td><td><input type="text" size="20" name="info" value="<?PHP echo $info; ?>"</td><td><input type="text" size="20" name="purpose" value="<?PHP echo $purpose; ?>"</td><td><input type="submit" value="update" class="btn btn-success"></form></td><td><form method="POST" action="serveredit.php"><input type="hidden" name="reset" value="<?PHP echo $id; ?>"><input type="submit" value="Reset" class="btn btn-warning"></td><td><form method="POST" action="serveredit.php"><input type="hidden" name="delete" value="<?PHP echo $id; ?>"><input type="submit" value="delete" class="btn btn-danger"></form></td></tr>
 
 <?PHP
