7c7feea283b30f919e16cc870f99cfed3cce38bb
danmed <danmed@gmail.com>
2017-05-25 14:23:56 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -33,7 +33,7 @@ if ($db_found)
 	$refresh = $_POST['refresh'];
 	$enable_smart = $_POST['enable_smart'];
 	$updatesql = "UPDATE config SET enablesmart = '". $enable_smart . "', smtp = '" . $smtp ."', smtp_port = '" . $smtp_port . "', smtp_username = '" . $smtp_username . "', smtp_password = '" . $smtp_password . "', admin_email = '" . $admin_email . "', refresh = '" . $refresh . "' where id = '1'";
-	
+}	
 ?>
 
     <script src="https://code.jquery.com/jquery.min.js"></script>
