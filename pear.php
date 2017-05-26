b6ebbf59694d418f29284e439885902b44420d30
danmed <danmed@gmail.com>
2017-05-26 15:36:01 +0100

    Create pear.php

---
+++ pear.php
@@ -0,0 +1,28 @@
+<?php
+
+error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
+
+set_include_path("." . PATH_SEPARATOR . ($UserDir = dirname($_SERVER['DOCUMENT_ROOT'])) . "/pear/php" . PATH_SEPARATOR . get_include_path());
+require_once "Mail.php";
+
+$host = "smtp.gmail.com";
+$username = "danmed@gmail.com";
+$password = "N01d34M4t3";
+$port = "567";
+$to = "dan.medhurst@planb.co.uk";
+$email_from = "danmed@gmail.com";
+$email_subject = "Subject Line Here: " ;
+$email_body = "whatever you like" ;
+$email_address = "reply-to@example.com";
+
+$headers = array ('From' => $email_from, 'To' => $to, 'Subject' => $email_subject, 'Reply-To' => $email_address);
+$smtp = Mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));
+$mail = $smtp->send($to, $headers, $email_body);
+
+
+if (PEAR::isError($mail)) {
+echo("<p>" . $mail->getMessage() . "</p>");
+} else {
+echo("<p>Message successfully sent!</p>");
+}
+?>
