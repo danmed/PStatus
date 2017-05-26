36d078c610f3752afc980a36782c3701884fd39d
Karl Bastiman <ksb@amberweb01.awphost.org>
2017-05-26 15:02:42 +0100

    test

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?PHP
+# ksb
 echo "<font size=1>Design and code by Dan Medhurst 2017</font><br>";
 echo "<font size=1>Icons made by <a href='http://www.flaticon.com/authors/icon-works'>Icon Works</a> from <a href='http://www.flaticon.com'>www.flaticon.com</a></font>";
 ?>
