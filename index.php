45bf6fc448bf88e6d8f0c9fb19a5d1222d1b3512
danmed <danmed@gmail.com>
2017-05-26 21:08:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $refresh = $_GET['refresh'];
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css"></style>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
 <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>	 
 	  
     <!--[if lt IE 9]>
