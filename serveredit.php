41c3c3382399747ae9e571a210cebda3d7f37875
danmed <danmed@gmail.com>
2017-05-26 23:07:30 +0100

    Update serveredit.php

--- serveredit.php
+++ serveredit.php
@@ -7,6 +7,7 @@
 	<meta http-equiv="refresh" content="20">
     <title>PStatus</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
+	      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	      <script src="js/bootstrap.min.js"></script>	 
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
