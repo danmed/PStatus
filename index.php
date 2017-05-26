48b8c8f81c709e18e09ebf91784f1752046648c3
danmed <danmed@gmail.com>
2017-05-26 20:37:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,11 +15,6 @@ $refresh = $_GET['refresh'];
     <title>PStatus</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
 	  
-<link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">   
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css"></style>
-<script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
-<script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -39,6 +34,8 @@ $refresh = $_GET['refresh'];
 
     <?PHP include "navbar.php"; ?>
 
+<link rel="stylesheet" href="js/jquery.dataTables.min.css"></style>
+<script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
