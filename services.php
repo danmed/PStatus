a1ca55c5df2beab8cacaf70940a1ac181e02486e
danmed <danmed@gmail.com>
2017-05-28 19:01:55 +0100

    Update services.php

--- services.php
+++ services.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="refresh" content="20">
+    <meta http-equiv="refresh" content="20">
     <title>PStatus</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!--[if lt IE 9]>
@@ -14,26 +14,30 @@
   <style>
   .progress {
     margin-bottom: 0 !important;
-	background-color: #DA2A2A;
+    background-color: #DA2A2A;
     -webkit-box-shadow: none;
     box-shadow: none;
 }
 
 </style>
-	</head>
+    </head>
 
   <body>
 
-    <?PHP include "navbar.php"; ?>
-	  <?PHP include "config.inc.php"; ?>
+    <?PHP
+include "navbar.php";
+?>
+     <?PHP
+include "config.inc.php";
+?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-	<script type="text/javascript">
+    <script type="text/javascript">
 
 $(document).ready(function(){
     $("#status td.on_off:contains('false')").css('background-color','#E05667');
-	$("#status td.on_off:contains('true')").css('background-color','#56E08E');
+    $("#status td.on_off:contains('true')").css('background-color','#56E08E');
 });
 
 </script>
@@ -43,38 +47,39 @@ $(document).ready(function(){
 <?PHP
 $parent = $_GET['parent'];
 $device = $_GET['device'];
-$ip = $_GET['ip'];
+$ip     = $_GET['ip'];
 ?>
 
 <table class="table" id="status" cellpadding="4" cellspacing="4" border="1">
 <thead>
-<tr><th colspan="4"><center><b><img src="icons/001-window.png">&nbsp;Service Ping Status for <?PHP echo $device; ?></th></tr>
+<tr><th colspan="4"><center><b><img src="icons/001-window.png">&nbsp;Service Ping Status for <?PHP
+echo $device;
+?></th></tr>
 </thead>
 <tr><td><b>DEVICE</td><td><b>PORT</td><td><b>STATUS</td></tr>
 <?PHP
 
 $db_handle = mysqli_connect($DBServer, $DBUser, $DBPassword);
-$db_found = mysqli_select_db($db_handle, 'status');
+$db_found  = mysqli_select_db($db_handle, 'status');
 
 
-if ($db_found) 
-{
-$SQL = "select * from services where parent = '" . $parent . "'"	;
-$result = mysqli_query($db_handle, $SQL);
-while ($db_field = mysqli_fetch_assoc($result))
-{
-	$name = $db_field['name'];
-	$port = $db_field['port'];
-	$online  = servicetest($ip, $port);
-
-	print "<tr><td>" . $name . "</td><td>" . $port . "</td><td class='on_off'>" . ($online ? 'true':'false') . "</td></tr>";
+if ($db_found) {
+    $SQL    = "select * from services where parent = '" . $parent . "'";
+    $result = mysqli_query($db_handle, $SQL);
+    while ($db_field = mysqli_fetch_assoc($result)) {
+        $name   = $db_field['name'];
+        $port   = $db_field['port'];
+        $online = servicetest($ip, $port);
+        
+        print "<tr><td>" . $name . "</td><td>" . $port . "</td><td class='on_off'>" . ($online ? 'true' : 'false') . "</td></tr>";
+    }
+    
+    mysqli_close($db_handle);
+    
 }
 
-mysqli_close($db_handle);
-	
-}
-
-function servicetest($ip, $port) {
+function servicetest($ip, $port)
+{
     $socket = @fsockopen($ip, $port, $errorNo, $errorStr, 3);
     if ($errorNo == 0) {
         return true;
@@ -87,8 +92,12 @@ mysqli_close($db_handle);
 
 ?>
 
-	</table>
-	<?PHP include "aboutmodal.php"; ?>
-	<?PHP include "footer.php"; ?>
-	</body>
-	</html>
+    </table>
+    <?PHP
+include "aboutmodal.php";
+?>
+   <?PHP
+include "footer.php";
+?>
+   </body>
+    </html>
