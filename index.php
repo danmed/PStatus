414ca09a8a161ce746dd1a5e2202b20c00eeec81
danmed <danmed@gmail.com>
2017-05-29 13:27:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -132,18 +132,7 @@ function pingtest($ip)
 }
 
 ?>
-   <script>
-        var myVar;
 
-function myFunction() {
-    myVar = setTimeout(showPage, 3000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
-    </script>
 
     <br>
     <?PHP
