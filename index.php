b49b9e260d8c740b2c9cd632f8c4983c82c0aba2
danmed <danmed@gmail.com>
2017-06-01 13:31:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ $(document).ready(function() {
     </script>
     <meta http-equiv="refresh" content="<?PHP
 echo $refresh;
-?>";url="index.php">
+?>";url="http://www.google.co.uk">
     <title>PStatus</title>
 
 <style>    
