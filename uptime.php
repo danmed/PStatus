4f3240b585fa994145606840d0d08c45a5a1db7a
danmed <danmed@gmail.com>
2017-10-24 12:33:29 +0100

    Update uptime.php

--- uptime.php
+++ uptime.php
@@ -48,7 +48,7 @@ if ($db_found) {
                 error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
                 set_include_path("." . PATH_SEPARATOR . ($UserDir = dirname($_SERVER['DOCUMENT_ROOT'])) . "/pear/php" . PATH_SEPARATOR . get_include_path());
                 require_once "Mail.php";
-                $host          = ssl://'.$smtp;
+                $host          = 'ssl://'.$smtp;
                 $port          = $smtp_port;
                 $to            = $admin_email;
                 $email_from    = $smtp_username;
