59a2cedf6bbf8eb1821ddc5f9be03dc760cedc85
danmed <danmed@gmail.com>
2017-10-26 15:09:33 +0100

    Update sendpush.php

--- sendpush.php
+++ sendpush.php
@@ -14,25 +14,21 @@ if ($db_found) {
     $SQL    = "select * from pushover where id = '1' ";
     $result = mysqli_query($db_handle, $SQL);
     while ($db_field = mysqli_fetch_assoc($result)) {
-
-        
-echo $db_field['setToken'];   
-echo $db_field['SetUser'];
-        
+  
 $push = new Pushover();
 $push->setToken($db_field['setToken']); //App Token
-$push->setUser($db_field['setUser']); // User Token
+$push->setUser($db_field['SetUser']); // User Token
 
 $push->setTitle($_GET['Title']);
 $push->setMessage($_GET['Message']);
 
-$push->setPriority($db_field['setPriority']);
-$push->setRetry($db_field['setRetry']); //Used with Priority = 2; Pushover will resend the notification every 60 seconds until the user accepts.
-$push->setExpire($db_field['setExpire']); //Used with Priority = 2; Pushover will resend the notification every 60 seconds for 3600 seconds. After that point, it stops sending notifications.
-$push->setCallback($db_field['setCallback']);
+$push->setPriority($db_field['SetPriority']);
+$push->setRetry($db_field['SetRetry']); //Used with Priority = 2; Pushover will resend the notification every 60 seconds until the user accepts.
+$push->setExpire($db_field['SetExpire']); //Used with Priority = 2; Pushover will resend the notification every 60 seconds for 3600 seconds. After that point, it stops sending notifications.
+$push->setCallback($db_field['SetCallback']);
 $push->setTimestamp(time());
 $push->setDebug(true);
-$push->setSound($db_field['setSound']);
+$push->setSound($db_field['SetSound']);
 
 $go = $push->send();
 
