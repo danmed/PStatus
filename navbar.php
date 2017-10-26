d147a61c73f8ef06ae483c102908a5811a04a2f4
danmed <danmed@gmail.com>
2017-10-26 09:53:41 +0100

    Removing Refresh option

--- navbar.php
+++ navbar.php
@@ -13,23 +13,6 @@
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#" data-toggle="modal" data-target="#aboutmodal">About</a></li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Refresh<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="index.php?refresh=5">5 seconds</a></li>
-                        <li><a href="index.php?refresh=10">10 seconds</a></li>
-                        <li><a href="index.php?refresh=15">15 seconds</a></li>
-                        <li><a href="index.php?refresh=20">20 seconds</a></li>
-                        <li><a href="index.php?refresh=25">25 seconds</a></li>
-                        <li><a href="index.php?refresh=30">30 seconds</a></li>
-                        <li><a href="index.php?refresh=35">35 seconds</a></li>
-                        <li><a href="index.php?refresh=40">40 seconds</a></li>
-                        <li><a href="index.php?refresh=45">45 seconds</a></li>
-                        <li><a href="index.php?refresh=50">50 seconds</a></li>
-                        <li><a href="index.php?refresh=55">55 seconds</a></li>
-                        <li><a href="index.php?refresh=60">60 seconds</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin<span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="serveradd.php">Add Devices</a></li>
